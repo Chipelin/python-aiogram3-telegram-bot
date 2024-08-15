2cf4e6d2b111cfdba2ec80fcfdf781849516c5f9
Chipelin <mrchipelin@gmail.com>
2024-08-16 04:50:34 +0800

    Reworked the for_starters keyboard as a function

--- handlers/start.py
+++ handlers/start.py
@@ -1,12 +1,12 @@
 from aiogram import Router, types, F
 from aiogram.filters import Command
-from keyboards.for_start import builder
+from keyboards.for_start import get_choices_kb
 
 router = Router()
 
 @router.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Добро пожаловать в наш бот!", reply_markup=builder.as_markup())
+    await message.answer("Добро пожаловать в наш бот!", reply_markup=get_choices_kb())
 
 @router.callback_query(F.data == "choice_one")
 async def choice_one_button(callback: types.CallbackQuery):
--- keyboards/for_start.py
+++ keyboards/for_start.py
@@ -1,13 +1,14 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from aiogram.types import InlineKeyboardButton        
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup        
 
+def get_choices_kb() -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
 
-builder = InlineKeyboardBuilder()
+    builder.add(InlineKeyboardButton(
+    text="Выбор 1",
+    callback_data="choice_one"))
 
-builder.add(InlineKeyboardButton(
-text="Выбор 1",
-callback_data="choice_one"))
-
-builder.add(InlineKeyboardButton(
-text="Выбор 2",
-callback_data="choice_two"))
+    builder.add(InlineKeyboardButton(
+    text="Выбор 2",
+    callback_data="choice_two"))
+    return builder.as_markup(resize_keyboard=True)
